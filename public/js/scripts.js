98dabed798f46a7d6296dc95a00bfdf7246b0266
andreyromanov <andreygorogogo@gmail.com>
2019-02-01 22:05:03 +0200

    Fixed error with forecast select

--- public/js/scripts.js
+++ public/js/scripts.js
@@ -1,9 +1,14 @@
+ 
+
   $(document).ready(function () {
+  	if (window.location.pathname == '/insert') {
   	$('html, body').animate({
   		scrollTop: $('#forc').offset().top
   	}, 'slow');
+  }
   });
 
   $(window).load(function() {
   	$(".se-pre-con").delay(1000).fadeOut("slow");
   });
+
